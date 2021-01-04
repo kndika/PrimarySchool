3945ef28e5b05c66d4a215818039f3a852ad7d03
kndika <indikaindika83@yahoo.com>
2021-01-04 22:41:16 +0530

    f
    f

---
+++ app/src/main/java/com/example/primaryschool/MainActivitySchool.kt
@@ -0,0 +1,10 @@
+package com.example.primaryschool
+
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+
+
+class MainActivitySchool: Fragment(R.layout.activity_main_school) {
+
+
+}
