62a23af4eeb255e807caf5ccc61f0baaa70ef553
kndika <indikaindika83@yahoo.com>
2021-01-09 14:24:35 +0530

    First commit

--- app/src/main/java/com/example/primaryschool/MainActivity.kt
+++ app/src/main/java/com/example/primaryschool/MainActivity.kt
@@ -4,6 +4,8 @@ package com.example.primaryschool
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,7 +16,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
          // top tool bara
+       // setSupportActionBar(findViewById(R.id.top_tool_bar))
+
         setSupportActionBar(findViewById(R.id.top_tool_bar))
+//        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navi_host) as NavHostFragment
+        navController = navHostFragment.navController
+        setupActionBarWithNavController(navController)
 
 
     }
