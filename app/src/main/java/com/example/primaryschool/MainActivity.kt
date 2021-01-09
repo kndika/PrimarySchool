300d81f42c5f097dff15c782ad9e773c777ab826
kndika <indikaindika83@yahoo.com>
2021-01-09 12:50:50 +0530

    First commit

--- app/src/main/java/com/example/primaryschool/MainActivity.kt
+++ app/src/main/java/com/example/primaryschool/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.primaryschool
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+         // top tool bara
+        setSupportActionBar(findViewById(R.id.top_tool_bar))
+
+
     }
+
+
 }
--- app/src/main/java/com/example/primaryschool/MainActivitySchool.kt
+++ app/src/main/java/com/example/primaryschool/MainActivitySchool.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 
 
 class MainActivitySchool: Fragment(R.layout.activity_main_school) {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.activity_main_school, container, false)
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<Button>(R.id.goinbutton).setOnClickListener {
+            view.findNavController().navigate(R.id.action_mainActivitySchool_to_mainManu)
+        }
     }
 }
